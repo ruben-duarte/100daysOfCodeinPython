3ce04b2336455c2262748e052680d958ea992681
Ruben Dario Duarte Anaya <ruben.duarte.anaya@gmail.com>
2022-12-16 20:15:10 -0500

    day 5

--- day4part2.py
+++ day4part2.py
@@ -45,3 +45,5 @@ print(suma)
 n = 100
 suma = n * (n+1) / 2
 print(suma)
+
+
---
+++ day5 part1.py
@@ -0,0 +1,68 @@
+5  +5
+10
+print("Hey mundo")
+
+print("*"*5)
+
+print("*" + " "*3 + "*")
+
+print("*" + " "*6 + "*")
+
+print("*" + " "*7 + "*")
+
+columns =  "*" + " "*7 + "*"
+row = "*"*5
+n = 3
+for number in range(8):
+    if number == n+1:
+        print(row)
+    else:
+        print(columns)
+
+num = 5
+num = 7
+num + num
+
+num_visitas = 24
+nombre_completo ='Carlos Jeronimo'
+_es_numero = False
+edad = 16
+dolares = 377.8
+temperatura = -3
+
+type(num_visitas)
+type(nombre_completo)
+type(_es_numero)
+type(dolares)
+len(nombre_completo)
+
+for number in range(7):
+    if number == n:
+        print(row)
+    else:
+        print(columns)
+
+
+columns =  "*" + " "*7 + "*"
+row = "*"*9
+
+n = 4
+
+print(' ')
+for number in range(7):
+    if number == n-1:
+        print(row)
+    else:
+        print(columns)
+
+print(' ')
+
+print(' ')
+for number in range(7):
+    if number == 0 or number == 6:
+        print(row)
+    else:
+        print(columns)
+
+print(' ')
+
